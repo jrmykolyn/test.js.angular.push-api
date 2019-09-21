b144bddd37f6cba2492ff37ee5c9ad978d9f2e1a
Jesse Mykolyn <jrmykolyn@gmail.com>
2019-09-21 01:18:29 -0400

    Update subscriptions endpoint to support GET and POST

--- src/routes/subscriptions.js
+++ src/routes/subscriptions.js
@@ -1,18 +1,31 @@
 const db = require('../db');
 
 const subscriptions = (req, res, cb) => {
-  const body = [];
+  switch (req.method) {
+    case 'GET':
+      db.subscriptions.find({}, (err, subs) => {
+        if (err) return cb(new Error('Failed to retrieve subscriptions.'));
+        const subIds = subs.map((sub) => ({ id: sub.id }));
+        return cb(null, JSON.stringify(subIds));
+      });
+      break;
+    case 'POST':
+      const body = [];
 
-  req.on('data', (chunk) => {
-    body.push(chunk);
-  });
+      req.on('data', (chunk) => {
+        body.push(chunk);
+      });
 
-  req.on('end', () => {
-    db.subscriptions.insert(JSON.parse(body.toString()), (err, data) => {
-      if (err) return cb(err);
-      return cb(null, data);
-    });
-  });
+      req.on('end', () => {
+        db.subscriptions.insert(JSON.parse(body.toString()), (err, data) => {
+          if (err) return cb(err);
+          return cb(null, '');
+        });
+      });
+      break;
+    default:
+      return cb(new Error('Failed to match request type: ' + req.method + '.'));
+  }
 };
 
 module.exports = subscriptions;
